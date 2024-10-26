b9da73433023570c03cf18b514fe50a1eef0bc73
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-26 16:49:41 -0300

    Updated home section and styles from it. Implementing start function and style for testing

---
+++ src/sections/home/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function Home() {
+    return (
+        <section className={styles.section}>
+            <h1>Home</h1>
+        </section>
+    )
+}
