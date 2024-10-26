9826fa1a0e3b8732b6030d7190ba4394ab4f7e2b
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-26 18:59:10 -0300

    Updated navbar component. Changed 'About' link to 'About Us'

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
                 </Link>
                 <section className={styles.nav_container_linksSection}>
                     <Link to="home" smooth={true} duration={500} className={styles.links}>Home</Link>
-                    <Link to="about" smooth={true} duration={500} className={styles.links}>About</Link>
+                    <Link to="about" smooth={true} duration={500} className={styles.links}>About Us</Link>
                     <Link to="services" smooth={true} duration={500} className={styles.links}>Services</Link>
                     <Link to="contact" smooth={true} duration={500} className={styles.links}>Contact</Link>  
                 </section>
