df28125ac940ad472af8c9f75c02cc9f734adac9
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-26 18:53:29 -0300

    Updated home section. Added home id

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 export default function Home() {
     return (
-        <section className={styles.section}>
+        <section className={styles.section} id='home'>
             <h1>Home</h1>
         </section>
     )
