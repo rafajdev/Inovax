032e3c46dd6da517abb28edcea0a44fe87fdc576
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-26 18:53:10 -0300

    Updated navbar component and styles. Added logo and links to every section

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,20 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css"
+import { Link } from 'react-scroll';
+
 export default function Navbar() {
     return (
         <nav className={styles.nav}>
-            <h1>Navbar</h1>
+            <div className={styles.nav_container}>
+                <Link to="home" smooth={true} duration={500} className={styles.links}>
+                    <img src="./logo.png" alt="logo" />
+                </Link>
+                <section className={styles.nav_container_linksSection}>
+                    <Link to="home" smooth={true} duration={500} className={styles.links}>Home</Link>
+                    <Link to="about" smooth={true} duration={500} className={styles.links}>About</Link>
+                    <Link to="services" smooth={true} duration={500} className={styles.links}>Services</Link>
+                    <Link to="contact" smooth={true} duration={500} className={styles.links}>Contact</Link>  
+                </section>
+            </div>
         </nav>
     )
 }
