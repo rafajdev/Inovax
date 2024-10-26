0335fa768943ef9bca56973ba0e9c2fade0df690
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-26 17:08:29 -0300

    Created navbar component an added testing styles

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,8 @@
+import styles from './styles.module.css'
+export default function Navbar() {
+    return (
+        <nav className={styles.nav}>
+            <h1>Navbar</h1>
+        </nav>
+    )
+}
