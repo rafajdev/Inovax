6c4bd138898e93d4c476ea193c7ede8add9033b4
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-27 15:15:12 -0300

    Created about us section and styles

---
+++ src/sections/aboutus/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css"
+
+export default function AboutUs() {
+    return (
+        <section className={styles.section} id='about'>
+            <div className={styles.section_content}>
+                <div className={styles.section_content_container}>
+
+                </div>
+            </div>
+        </section>
+    )
+}
