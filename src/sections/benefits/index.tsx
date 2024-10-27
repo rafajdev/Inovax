b00a6fa972a1c147a502638d555e5fa50504847d
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-27 20:56:16 -0300

    Updated button component. Changed colors

--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -11,10 +11,10 @@ export default function Benefits() {
                         <p className={styles.section_content_left_article_paragraph}>Inovax stands out as your premier technology partner, offering expertise  across industries, custom solutions tailored to your needs, a  client-centric approach that prioritizes your vision, cutting-edge  technology to keep you ahead, and scalable, sustainable solutions for  long-term growth. With a proven track record of successful projects and  satisfied clients, Inovax is the trusted partner you need to innovate,  transform, and thrive in today's digital landscape.</p>
                     </article>
                     <section className={styles.section_content_left_buttons}>
-                        <Button content='See our services' width='370px' backgroundColor='#536EF3' fontColor='#F8F8F8' backgroundColorHover='#485EC8' fontColorHover='#F8F8F8'/>
+                        <Button content='See our services' width='370px' backgroundColor='#536EF3' fontColor='#F8F8F8' backgroundColorHover='#3A4CA5' fontColorHover='#F8F8F8'/>
 
-                        <Button content='Contact us' width='370px' backgroundColor='transparent' fontColor='#080C23' backgroundColorHover='#F8F8F8' fontColorHover='#080C23'
-                        borderColor='#080C23' borderColorHover='#F8F8F8'
+                        <Button content='Contact us' width='370px' backgroundColor='transparent' fontColor='#080C23' backgroundColorHover='#080C23' fontColorHover='#F8F8F8'
+                        borderColor='#080C23' borderColorHover='#080C23'
                         />
                     </section>
                 </div>
