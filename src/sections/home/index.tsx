d5a042f9d0fde400d7ccf6d4be3843a398dfeb2e
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-27 14:51:46 -0300

    Updated home section and styles. Added the main content and styled it

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -3,7 +3,12 @@ import styles from './styles.module.css'
 export default function Home() {
     return (
         <section className={styles.section} id='home'>
-            <h1>Home</h1>
+            <div className={styles.section_content}>
+                <div className={styles.section_content_container}>
+                    <h1 className={styles.section_content_container_title}>IN<span className={styles.span}>O</span>VAX</h1>
+                    <p className={styles.section_content_container_subtitle}>Developing   innovative  solutions   for   your   success</p>
+                </div>
+            </div>
         </section>
     )
 }
