8a6b5b3cb12b1e98e73abdbfea93ca0d92050cb7
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-27 15:29:17 -0300

    Updated about us section and styles. Added image and margins for testing

--- src/sections/aboutus/index.tsx
+++ src/sections/aboutus/index.tsx
@@ -3,6 +3,7 @@ import styles from "./styles.module.css"
 export default function AboutUs() {
     return (
         <section className={styles.section} id='about'>
+            <img src="./people_working.png" alt="people working" className={styles.section_image}/>
             <div className={styles.section_content}>
                 <div className={styles.section_content_container}>
 
