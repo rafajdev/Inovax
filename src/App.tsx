949825c4ffd70363af65f3d7ca5e1cc582036ce6
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-30 21:44:37 -0300

    Updated 'App.tsx'. Added services section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar"
 import AboutUs from "./sections/aboutus"
 import Benefits from "./sections/benefits"
 import Home from "./sections/home"
+import Services from "./sections/ourservices"
 import "./styles/global.css"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <AboutUs />
       <Benefits />
+      <Services />
     </>
   )
 }
