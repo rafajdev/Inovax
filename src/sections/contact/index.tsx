ec26a0a4b3ae93cdd0f317c189702468f5061c0d
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-30 21:53:22 -0300

    Created contact section and styles

---
+++ src/sections/contact/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export default function Contact() {
+    return (
+        <section className={styles.section} id='contact'>
+            <div className={styles.section_content}>
+
+            </div>
+        </section>
+    )
+}
