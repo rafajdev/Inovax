1de69f9ca2905d1f0592a058621aeb94c2cfcac5
RafaR <rafadeoliveiramendes@gmail.com>
2024-11-01 22:10:51 -0300

    Updated contact section and styles. Added divs, title and first input

--- src/sections/contact/index.tsx
+++ src/sections/contact/index.tsx
@@ -1,9 +1,23 @@
+import Input from "../../components/input";
 import styles from "./styles.module.css";
 
 export default function Contact() {
     return (
         <section className={styles.section} id='contact'>
             <div className={styles.section_content}>
+                <div>
+                    <h1 className={styles.section_content_title}>Ready to take your business to the next level? 
+                    Contact us now!</h1>
+                </div>
+                <section>
+                    <Input type='text' placeholder='Full Name' height='75px'/>
+                </section>
+                <section>
+                    <div>
+                            
+                    </div>
+                </section>
+
 
             </div>
         </section>
