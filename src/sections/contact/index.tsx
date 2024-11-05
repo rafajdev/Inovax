1a5662325d988326b90a8d0ff5b6f3128626d9b1
RafaR <rafadeoliveiramendes@gmail.com>
2024-11-05 10:28:14 -0300

    Updated contact section and styles. Removed content for testing

--- src/sections/contact/index.tsx
+++ src/sections/contact/index.tsx
@@ -5,20 +5,12 @@ export default function Contact() {
     return (
         <section className={styles.section} id='contact'>
             <div className={styles.section_content}>
-                <div>
-                    <h1 className={styles.section_content_title}>Ready to take your business to the next level? 
-                    Contact us now!</h1>
+                <div className={styles.section_content_header}>
+                    <h1 className={styles.section_content_header_title}>Contact Us</h1>
                 </div>
-                <section>
-                    <Input type='text' placeholder='Full Name' height='75px'/>
-                </section>
-                <section>
-                    <div>
-                            
-                    </div>
-                </section>
-
-
+                <div>
+                    
+                </div>
             </div>
         </section>
     )
