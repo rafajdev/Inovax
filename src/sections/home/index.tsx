ee85e514d5fa26655c628ce12ca333d91d5466ab
RafaR <rafadeoliveiramendes@gmail.com>
2024-11-07 22:53:45 -0300

    Updated home section. Changed formatting

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -1,14 +1,18 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 export default function Home() {
-    return (
-        <section className={styles.section} id='home'>
-            <div className={styles.section_content}>
-                <div className={styles.section_content_container}>
-                    <h1 className={styles.section_content_container_title}>IN<span className={styles.span}>O</span>VAX</h1>
-                    <p className={styles.section_content_container_subtitle}>Developing   innovative  solutions   for   your   success</p>
-                </div>
+   return (
+      <section className={styles.section} id="home">
+         <div className={styles.section_content}>
+            <div className={styles.section_content_container}>
+               <h1 className={styles.section_content_container_title}>
+                  IN<span className={styles.span}>O</span>VAX
+               </h1>
+               <p className={styles.section_content_container_subtitle}>
+                  Developing innovative solutions for your success
+               </p>
             </div>
-        </section>
-    )
+         </div>
+      </section>
+   );
 }
