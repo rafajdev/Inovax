842d738c37dc4951468781cffa6b50cb11bb5ffe
RafaR <rafadeoliveiramendes@gmail.com>
2024-11-07 23:27:26 -0300

    Updated benefits and ourservices section. Added animations

--- src/sections/benefits/index.tsx
+++ src/sections/benefits/index.tsx
@@ -1,33 +1,106 @@
-import styles from './styles.module.css'
-import Button from '../../components/button'
-import InfoBlock from '../../components/infoblock'
+import styles from "./styles.module.css";
+import Button from "../../components/button";
+import InfoBlock from "../../components/infoblock";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default function Benefits() {
-    return (
-        <section className={styles.section} id='about'>
-            <div className={styles.section_content}>
-                <div className={styles.section_content_left}>
-                    <article className={styles.section_content_left_article}>
-                        <h2 className={styles.section_content_left_article_title}>Why Choose Inovax?</h2>
-                        <p className={styles.section_content_left_article_paragraph}>Inovax stands out as your premier technology partner, offering expertise  across industries, custom solutions tailored to your needs, a  client-centric approach that prioritizes your vision, cutting-edge  technology to keep you ahead, and scalable, sustainable solutions for  long-term growth. With a proven track record of successful projects and  satisfied clients, Inovax is the trusted partner you need to innovate,  transform, and thrive in today's digital landscape.</p>
-                    </article>
-                    <section className={styles.section_content_left_buttons}>
-                        <Button content='See our services' width='270px' backgroundColor='#536EF3' fontColor='#F8F8F8' backgroundColorHover='#3A4CA5' fontColorHover='#F8F8F8'/>
+   return (
+      <section className={styles.section} id="about">
+         <div className={styles.section_content}>
+            <div className={styles.section_content_left}>
+               <ScrollAnimation animateIn="fadeInLeft" animateOnce={true}>
+                  <article className={styles.section_content_left_article}>
+                     <h2 className={styles.section_content_left_article_title}>
+                        Why Choose Inovax?
+                     </h2>
+                     <p
+                        className={
+                           styles.section_content_left_article_paragraph
+                        }
+                     >
+                        Inovax stands out as your premier technology partner,
+                        offering expertise across industries, custom solutions
+                        tailored to your needs, a client-centric approach that
+                        prioritizes your vision, cutting-edge technology to keep
+                        you ahead, and scalable, sustainable solutions for
+                        long-term growth. With a proven track record of
+                        successful projects and satisfied clients, Inovax is the
+                        trusted partner you need to innovate, transform, and
+                        thrive in today's digital landscape.
+                     </p>
+                  </article>
+                  <section className={styles.section_content_left_buttons}>
+                     <Button
+                        content="See our services"
+                        width="270px"
+                        backgroundColor="#536EF3"
+                        fontColor="#F8F8F8"
+                        backgroundColorHover="#3A4CA5"
+                        fontColorHover="#F8F8F8"
+                     />
 
-                        <Button content='Contact us' width='270px' backgroundColor='transparent' fontColor='#080C23' backgroundColorHover='#080C23' fontColorHover='#F8F8F8'
-                        borderColor='#080C23' borderColorHover='#080C23'
-                        />
-                    </section>
-                </div>
-                <div className={styles.section_content_right}>
-                    <div className={styles.section_content_right_container}>
-                    <InfoBlock imageName='icon1.png' imageAlt='icon1' text='Custom Solutions'/>
-                    <InfoBlock imageName='icon2.png' imageAlt='icon2' text='Client-Centric Approach'/>
-                    <InfoBlock imageName='icon3.png' imageAlt='icon3' text='Expertise Across Industries'/>
-                    <InfoBlock imageName='icon4.png' imageAlt='icon4' text='Cutting-Edge Technology'/>
-                    </div>
-                </div>
+                     <Button
+                        content="Contact us"
+                        width="270px"
+                        backgroundColor="transparent"
+                        fontColor="#080C23"
+                        backgroundColorHover="#080C23"
+                        fontColorHover="#F8F8F8"
+                        borderColor="#080C23"
+                        borderColorHover="#080C23"
+                     />
+                  </section>
+               </ScrollAnimation>
             </div>
-        </section>
-    )
+
+            <div className={styles.section_content_right}>
+               <div className={styles.section_content_right_container}>
+                  <ScrollAnimation animateIn="flipInX" animateOnce={true}>
+                     <InfoBlock
+                        imageName="icon1.png"
+                        imageAlt="icon1"
+                        text="Custom Solutions"
+                     />
+                  </ScrollAnimation>
+
+                  <ScrollAnimation
+                     animateIn="flipInX"
+                     animateOnce={true}
+                     delay={200}
+                  >
+                     <InfoBlock
+                        imageName="icon2.png"
+                        imageAlt="icon2"
+                        text="Client-Centric Approach"
+                     />
+                  </ScrollAnimation>
+
+                  <ScrollAnimation
+                     animateIn="flipInX"
+                     animateOnce={true}
+                     delay={400}
+                  >
+                     <InfoBlock
+                        imageName="icon3.png"
+                        imageAlt="icon3"
+                        text="Expertise Across Industries"
+                     />
+                  </ScrollAnimation>
+
+                  <ScrollAnimation
+                     animateIn="flipInX"
+                     animateOnce={true}
+                     delay={600}
+                  >
+                     <InfoBlock
+                        imageName="icon4.png"
+                        imageAlt="icon4"
+                        text="Cutting-Edge Technology"
+                     />
+                  </ScrollAnimation>
+               </div>
+            </div>
+         </div>
+      </section>
+   );
 }
--- src/sections/ourservices/index.tsx
+++ src/sections/ourservices/index.tsx
@@ -1,5 +1,6 @@
 import Card from '../../components/card'
 import styles from './styles.module.css'
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default function Services() {
     return (
@@ -7,11 +8,17 @@ export default function Services() {
             <div className={styles.section_content}>
                 <h1 className={styles.section_content_title}>Our Services</h1>
                 <div className={styles.section_content_cards}>
-                    <Card title='Custom Solutions' titleBgColor='#E1EEFF' content="We create tailor-made software solutions to meet each client's specific needs, optimizing processes and improving operational efficiency." icon='icon5.png' iconAlt='icon5' buttonContent='Learn More' buttonColor='#E1EEFF' buttonHoverColer='#C0CEDF'/>
+                    <ScrollAnimation animateIn="fadeInLeft" delay={0.1} animateOnce={true}>
+                        <Card title='Custom Solutions' titleBgColor='#E1EEFF' content="We create tailor-made software solutions to meet each client's specific needs, optimizing processes and improving operational efficiency." icon='icon5.png' iconAlt='icon5' buttonContent='Learn More' buttonColor='#E1EEFF' buttonHoverColer='#C0CEDF'/>
+                    </ScrollAnimation>
 
-                    <Card title='Technology Upgrades' titleBgColor='#B0E0FD' content="We assist companies in upgrading their existing technologies by integrating modern tools and solutions that enhance efficiency and competitiveness." icon='icon6.png' iconAlt='icon6' buttonContent='View Details' buttonColor='#B0E0FD' buttonHoverColer='#93BBD4'/>
+                    <ScrollAnimation animateIn="fadeInUp" delay={0.2} animateOnce={true}>
+                        <Card title='Technology Upgrades' titleBgColor='#B0E0FD' content="We assist companies in upgrading their existing technologies by integrating modern tools and solutions that enhance efficiency and competitiveness." icon='icon6.png' iconAlt='icon6' buttonContent='View Details' buttonColor='#B0E0FD' buttonHoverColer='#93BBD4'/>
+                    </ScrollAnimation>
 
-                    <Card title='Process Automation' titleBgColor='#8EC2FF' content="We implement automation solutions to simplify routine tasks, reducing errors and freeing up resources for higher-value activities." icon='icon7.png' iconAlt='icon7' buttonContent='Get Details' buttonColor='#8EC2FF' buttonHoverColer='#6D96C6'/>
+                    <ScrollAnimation animateIn="fadeInRight" delay={0.3} animateOnce={true}>
+                        <Card title='Process Automation' titleBgColor='#8EC2FF' content="We implement automation solutions to simplify routine tasks, reducing errors and freeing up resources for higher-value activities." icon='icon7.png' iconAlt='icon7' buttonContent='Get Details' buttonColor='#8EC2FF' buttonHoverColer='#6D96C6'/>
+                    </ScrollAnimation>
                 </div>
             </div>
         </section>
