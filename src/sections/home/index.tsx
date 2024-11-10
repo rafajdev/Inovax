3e1d0933cdb52f100f680bf8dc184a915a455f8f
RafaR <rafadeoliveiramendes@gmail.com>
2024-11-10 00:01:19 -0300

    Updated home section. Changed classes

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -3,16 +3,14 @@ import styles from "./styles.module.css";
 export default function Home() {
    return (
       <section className={styles.section} id="home">
-         <div className={styles.section_content}>
-            <div className={styles.section_content_container}>
-               <h1 className={styles.section_content_container_title}>
+            <div className={styles.section_container}>
+               <h1 className={styles.section_container_title}>
                   IN<span className={styles.span}>O</span>VAX
                </h1>
-               <p className={styles.section_content_container_subtitle}>
+               <p className={styles.section_container_subtitle}>
                   Developing innovative solutions for your success
                </p>
             </div>
-         </div>
       </section>
    );
 }
