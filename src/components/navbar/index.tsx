0b36beaf3b0aa7e3ff7e5122aeb1db32fdb26a38
RafaR <rafadeoliveiramendes@gmail.com>
2024-11-09 21:06:43 -0300

    Updated navbar component. Navbar change on scroll logic changed

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,33 +1,73 @@
 import { useState } from "react";
-import styles from "./styles.module.css"
-import { Link } from 'react-scroll';
+import styles from "./styles.module.css";
+import { Link } from "react-scroll";
 
 export default function Navbar() {
-    const [nav, setNav] = useState(false);
+   const [nav, setNav] = useState(false);
 
-    const changeBackground = () => {
-        if (window.scrollY >= 187) {
-            setNav(true);
-        } else {
-            setNav(false)
-        }
-    }
+   const changeBackground = () => {
+      if (window.scrollY >= 187) {
+         setNav(true);
+      } else {
+         setNav(false);
+      }
+   };
 
-    window.addEventListener('scroll', changeBackground);
+   window.addEventListener("scroll", changeBackground);
 
-    return (
-        <nav className={nav ? styles.nav_active: styles.nav}>
-            <div className={styles.nav_container}>
-                <Link to="home" smooth={true} duration={500} className={styles.links}>
-                    <img src="./logo.png" alt="logo" />
-                </Link>
-                <section className={styles.nav_container_linksSection}>
-                    <Link to="home" smooth={true} duration={500} className={styles.links}>Home</Link>
-                    <Link to="about" smooth={true} duration={500} className={styles.links}>About Us</Link>
-                    <Link to="services" smooth={true} duration={500} className={styles.links}>Services</Link>
-                    <Link to="contact" smooth={true} duration={500} className={styles.links}>Contact</Link>  
-                </section>
+   return (
+      <nav className={nav ? styles.nav_active : styles.nav}>
+         <div className={styles.nav_container}>
+            <div>
+               <Link
+                  to="home"
+                  smooth={true}
+                  duration={500}
+                  className={styles.links}
+               >
+                  <img
+                     src="./logo.png"
+                     alt="logo"
+                     className={styles.nav_container_logo}
+                  />
+               </Link>
             </div>
-        </nav>
-    )
+
+            <section className={styles.nav_container_linksSection}>
+               <Link
+                  to="home"
+                  smooth={true}
+                  duration={500}
+                  className={styles.links}
+               >
+                  Home
+               </Link>
+               <Link
+                  to="about"
+                  smooth={true}
+                  duration={500}
+                  className={styles.links}
+               >
+                  About Us
+               </Link>
+               <Link
+                  to="services"
+                  smooth={true}
+                  duration={500}
+                  className={styles.links}
+               >
+                  Services
+               </Link>
+               <Link
+                  to="contact"
+                  smooth={true}
+                  duration={500}
+                  className={styles.links}
+               >
+                  Contact
+               </Link>
+            </section>
+         </div>
+      </nav>
+   );
 }
